41178de42c1710a2420879bf7f77dd9b9443e0ef
MuhamadHaydar <49116664+MuhamadHaydar@users.noreply.github.com>
2020-05-17 17:07:54 +0300

    Add method to show title movies.

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,23 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Random;
+import java.util.Scanner;
 
 // Simple class for game operations.
 public class Game {
+
+    // Get the file of movie titles.
+    File movie = new File("movie_titles.txt");
+
+    // Use scanner to read through the files.
+    Scanner scanner;
+
+    // Method shows the title of movies.
+    public void showTitle(){
+        while (scanner.hasNext()){
+            String movieTitle = scanner.nextLine();
+            System.out.println(movieTitle);
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     // Main method.
     public static void main (String [] args){
-        // Do nothing.
+        Game game = new Game();
+        game.showTitle();
     }
 }
