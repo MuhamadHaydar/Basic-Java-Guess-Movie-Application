632595377485ad2b44b259fd02da43ad59b8553a
MuhamadHaydar <49116664+MuhamadHaydar@users.noreply.github.com>
2020-05-17 17:23:21 +0300

    Edit codebase of Game class and Add constructor

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -12,12 +14,43 @@ public class Game {
     // Use scanner to read through the files.
     Scanner scanner;
 
-    // Method shows the title of movies.
-    public void showTitle(){
+    // Save the movie titles.
+    LinkedList<String> movieTitles = new LinkedList<>();
+
+    //Game Constructor.
+    Game(){
+        // initialize scanner through the main constructor.
+        {
+            try {
+                scanner = new Scanner(movie);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // add whole movie.
+        addMovieTitles();
+    }
+
+    // Add whole movie titles to linked list
+    private void addMovieTitles (){
         while (scanner.hasNext()){
             String movieTitle = scanner.nextLine();
-            System.out.println(movieTitle);
+            movieTitles.add(movieTitle);
         }
+    }
+
+    // Method shows the title of movies.
+    public void showTitle(){
+        for (int i = 0; i<movieTitles.size(); i ++){
+            System.out.println(movieTitles.get(i));
+        }
+    }
+
+    //Pick one movie randomly.
+    public void pickOneMovie(){
+
+
     }
 
 }
